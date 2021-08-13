d6c55ca6223a3b69fa22ee297aab27964daf81be
JH <qasdqqqqqq>
2021-08-13 17:55:52 +0900

    cmaketest

---
+++ lab10/cmaketest/CMakeFiles/fx_main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pc-u17/practical_c/lab10/cmaketest/fx_main.c" "/home/pc-u17/practical_c/lab10/cmaketest/CMakeFiles/fx_main.dir/fx_main.c.o"
+  "/home/pc-u17/practical_c/lab10/cmaketest/fx_s2308.c" "/home/pc-u17/practical_c/lab10/cmaketest/CMakeFiles/fx_main.dir/fx_s2308.c.o"
+  "/home/pc-u17/practical_c/lab10/cmaketest/fx_test.c" "/home/pc-u17/practical_c/lab10/cmaketest/CMakeFiles/fx_main.dir/fx_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab10/cmaketest/CMakeFiles/fx_main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fx_main.dir/fx_main.c.o"
+  "CMakeFiles/fx_main.dir/fx_test.c.o"
+  "CMakeFiles/fx_main.dir/fx_s2308.c.o"
+  "fx_main.pdb"
+  "fx_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fx_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
